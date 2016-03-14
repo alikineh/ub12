ab5abf244b2855c00747377d480af05e2441619c
alikineh <000xxx000information000xxx000@gmail.com>
2016-03-14 17:04:29 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = " "--توکن بوت را درون " قرار دهید --
+local bot_api_key = "212180108:AAEdcZnk5uGo-azgH3dpmXXKyT_oXG3JcgY"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[
